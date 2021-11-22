00663a601739bd7f14023f00e4b30baa9d87b2a1
youyy <youyy@glodon.com>
2021-11-22 16:34:51 +0800

    修改配置

---
+++ spring-context/src/main/java/com/youyy/bean/MyBean.java
@@ -0,0 +1,26 @@
+package com.youyy.bean;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class MyBean {
+	private String username = "elephant";
+
+	private String password;
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+}
